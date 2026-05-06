470bbfa84f7378be66a084f39a8aaf6cd05b29c0
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-05-07 03:25:49 +0530

    Add model exports

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.schemas import CandidateProfile, EvidenceMatch, FitScore, JobDescription, Project, ResumeRules
+
+__all__ = [
+    "CandidateProfile",
+    "EvidenceMatch",
+    "FitScore",
+    "JobDescription",
+    "Project",
+    "ResumeRules",
+]
